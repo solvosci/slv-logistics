4dc70fafe6698b5f0e6975a506a63d849e1b900e
David Alonso // Solvos <david.alonso@solvos.es>
2026-08-31 17:19:59 +0200

    [MIG] logistics_planning_purchase: restore manifest unneeded changes

--- logistics_planning_purchase/__manifest__.py
+++ logistics_planning_purchase/__manifest__.py
@@ -10,16 +10,16 @@
     'version': '17.0.1.0.0',
     'category': 'stock',
     'website': 'https://github.com/solvosci/slv-logistics',
-    'depends': [
-        'purchase_stock',
-        'logistics_planning_invoicing',
+    "depends": [
+        "purchase_stock",
+        "logistics_planning_invoicing",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/logistics_schedule_views.xml',
-        'views/purchase_order_views.xml',
-        'views/stock_picking_type_views.xml',
-        'views/res_config_settings_views.xml',
-        'wizards/logistics_schedule_purchase_add_wizard_views.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "views/logistics_schedule_views.xml",
+        "views/purchase_order_views.xml",
+        "views/stock_picking_type_views.xml",
+        "views/res_config_settings_views.xml",
+        "wizards/logistics_schedule_purchase_add_wizard_views.xml",
     ],
 }
