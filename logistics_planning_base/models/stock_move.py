b1d8da64008abb3cf129edf4f86ff1c5a1428802
ChristianSantamaria <christiancameselle@hotmail.com>
2026-09-01 08:40:40 +0200

    [FIX] logistics_planning_base: name_search and name_get

--- logistics_planning_base/models/stock_move.py
+++ logistics_planning_base/models/stock_move.py
@@ -43,20 +43,13 @@ class StockMove(models.Model):
             (self.picking_id.name, self.product_uom_qty, self.product_uom.name)
         )
 
-    def name_get(self):
+    @api.depends("picking_id.name", "product_uom_qty", "product_uom.name")
+    def _compute_display_name(self):
         if self.env.context.get("logistics_schedule_view", False):
-            result = []
-            for rec in self:
-                # name = (
-                #     "%s (%.3f %s)"
-                #     %
-                #     (rec.picking_id.name, rec.product_uom_qty, rec.product_uom.name)
-                # )
-                # result.append((rec.id, name))
-                result.append((rec.id, rec._prepare_name_get()))
-            return result
+            for record in self:
+                record.display_name = record._prepare_name_get()
         else:
-            return super().name_get()
+            super(StockMove, self)._compute_display_name()
 
     @api.model
     def _name_search(self, name, domain=None, operator="ilike", limit=None, order=None):
@@ -65,19 +58,22 @@ class StockMove(models.Model):
         and sorted by picking
         """
         if self.env.context.get("logistics_schedule_view", False) and name:
-            args = args or []
-            domain = [
+            domain = domain or []
+            extra_domain = [
                 "|",
                 ("picking_id.name", operator, name),
                 "|",
                 ("product_id.default_code", operator, name),
                 ("product_id.name", operator, name),
             ]
-            rec = self._search(
-                expression.AND([domain, args]), limit=limit,
+            rec_ids = self._search(
+                expression.AND([extra_domain, domain]), limit=limit, order=order,
             )
-            return models.lazy_name_get(
-                self.browse(rec).sorted(key=lambda x: (x.picking_id.name))
+            records = self.browse(rec_ids).sorted(
+                key=lambda x: (x.picking_id.name or "")
             )
-        else:
-            return super()._name_search(name=name, domain=domain, operator=operator, limit=limit, order=order)
+            return records.ids
+
+        return super()._name_search(
+            name=name, domain=domain, operator=operator, limit=limit, order=order
+        )
